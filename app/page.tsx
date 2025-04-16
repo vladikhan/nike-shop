49489b6b4e5485afab5834f85682fc8d83b33a1e
Vladimir Khan <vencedor843@gmail.com>
2025-04-17 04:24:08 +0900

    Initial commit 💥

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-lg">title 1</h1>
+      <h2>title 2</h2>
+      <h3>title 3</h3>
+      <p>a regular paragraph</p>
+      <p>a description paragraph</p>
+      <p className="note">a little note</p>
+    </div>
+  )
+}
